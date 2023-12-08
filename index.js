cc85892e7b712d6a7a3c8220b4af62b02f0c6981
sandeeep-prajapti <6392424180sandeep@gmail.com>
2023-12-08 10:25:26 +0530

    area calculation

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const a=20;
-a = 10;
-console.log(a)
+// const a=20;
+// a = 10;
+// console.log(a)
 
-let a=20;
-a = 10;
-console.log(a)
+// let b=20;
+// b = 10;
+// console.log(b)
 
-var a=20;
-a = 10;
-console.log(a)
+// var c=20;
+// c = 10;
+// console.log(c)
+
+var arm = 4;
+const pi = 3.14;
+console.log(`area of triangle is  : ${pi*arm*arm}`)
+
+var arm2 = 5;
+var arm1 = 4;
+console.log(`area of react is  : ${arm1*arm2}`)
+
+var arm1 = 4;
+console.log(`area of react is  : ${arm1*arm1}`)
