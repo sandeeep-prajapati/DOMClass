ce18ceae1557a227731b951b985024534ae80131
sandeeep-prajapti <6392424180sandeep@gmail.com>
2023-12-08 10:45:24 +0530

    shhjygjs

--- index.js
+++ index.js
@@ -10,18 +10,57 @@
 // c = 10;
 // console.log(c)
 
-var arm = 4;
+var arm = 8;
 const pi = 3.14;
-console.log(`area of triangle is  : ${pi*arm*arm}`)
-console.log(`parameter of triangle is  : ${3*arm}`)
+console.log(`area of circal is  : ${pi*arm*arm}`)
+console.log(`parameter of circal is  : ${2*pi*arm}`)
+var arm = 7;
+console.log(`area of circal is  : ${pi*arm*arm}`)
+console.log(`parameter of circal is  : ${2*pi*arm}`)
+var arm = 12;
+console.log(`area of circal is  : ${pi*arm*arm}`)
+console.log(`parameter of circal is  : ${2*pi*arm}`)
+var arm = 14;
+console.log(`area of circal is  : ${pi*arm*arm}`)
+console.log(`parameter of circal is  : ${2*pi*arm}`)
+var arm = 6;
+console.log(`area of circal is  : ${pi*arm*arm}`)
+console.log(`parameter of circal is  : ${2*pi*arm}`)
+var arm = 4;
+console.log(`area of circal is  : ${pi*arm*arm}`)
+console.log(`parameter of circal is  : ${2*pi*arm}`)
 
+// rect
 
-var arm2 = 5;
+var arm2 = 8;
+var arm1 = 7;
+console.log(`area of react is  : ${arm1*arm2}`)
+console.log(`parameter of react is  : ${2*(arm1+arm2)}`)
+
+var arm2 = 7;
+var arm1 = 12;
+console.log(`area of react is  : ${arm1*arm2}`)
+console.log(`parameter of react is  : ${2*(arm1+arm2)}`)
+
+var arm2 = 14;
+var arm1 = 12;
+console.log(`area of react is  : ${arm1*arm2}`)
+console.log(`parameter of react is  : ${2*(arm1+arm2)}`)
+
+var arm2 = 14;
+var arm1 = 6;
+console.log(`area of react is  : ${arm1*arm2}`)
+console.log(`parameter of react is  : ${2*(arm1+arm2)}`)
+
+var arm2 = 6;
 var arm1 = 4;
 console.log(`area of react is  : ${arm1*arm2}`)
 console.log(`parameter of react is  : ${2*(arm1+arm2)}`)
 
 
-var arm1 = 4;
-console.log(`area of react is  : ${arm1*arm1}`)
-console.log(`parameter of react is  : ${4*arm1}`)
+
+
+
+var arm1 = 14;
+console.log(`area of square is  : ${arm1*arm1}`)
+console.log(`parameter of square is  : ${4*arm1}`)
